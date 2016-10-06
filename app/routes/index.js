9e351ec12bd4acbac34fb9dfe9d97678cf35db20
Marko Stijak <mstijak@gmail.com>
2016-10-06 15:56:55 +0200

    New release

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,8 @@ import {HtmlElement} from 'cx/ui/HtmlElement';
 export default <cx>
     <div>
         <div class="b-credits" preserveWhitespace>
-            Comparison views on the data of <a href="http://stateofjs.com/">The State of JavaScript 2016</a> survey
-            implemented using <a href="http://cx.codaxy.com/">Cx</a>.
+            Presentation of <a href="http://stateofjs.com/">The State of JavaScript 2016</a> survey
+            results using <a href="http://cx.codaxy.com/">Cx</a>.
         </div>
         <Default/>
     </div>
